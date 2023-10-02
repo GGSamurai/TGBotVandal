6d696df744fe5e78a53fe9247badb329c1869165
GGSamurai <viliamazovskij@gmail.com>
2023-10-02 03:32:42 +0300

    delete s in line 44

---
+++ keyboard.py
@@ -0,0 +1,44 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
+main_kb = ReplyKeyboardMarkup(
+    keyboard =[
+        [
+            KeyboardButton(text = "Районы"),
+            KeyboardButton(text = "Показать адреса")
+        ],
+        [
+            KeyboardButton(text = "Удалить объект"),
+            KeyboardButton(text = "Инфо о командах")
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder='Выберите действие из меню',
+    selective=True
+)
+
+area_kb = ReplyKeyboardMarkup(
+    keyboard = [
+        [
+            KeyboardButton(text = "Ворошиловский"),
+            KeyboardButton(text = "Центральный")
+        ],
+        [
+            KeyboardButton(text = "Краснооктябрьский")
+        ],
+        [
+            KeyboardButton(text = "Советский"),
+            KeyboardButton(text = "Кировский")
+        ]
+    ],
+    resize_keyboard=True,
+    one_time_keyboard=True,
+    input_field_placeholder='Выберите район из меню',
+    selective=True
+)
+s
