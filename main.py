238bfa745a7a88bf0c495ba1bcc8c9a177feb171
GGSamurai <viliamazovskij@gmail.com>
2023-10-11 19:17:53 +0300

    Add function in menu street

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import var
 bot = Bot('6682215796:AAEall5HxrX3328CmjhcO0I7iRgNvWBs9nk', parse_mode="HTML")
 dp = Dispatcher()
 area = var.start_area
+mass = ['Ворошиловский']
 
 
 @dp.message(Command("start")) #Ответ на команду /start
@@ -28,10 +29,26 @@ async def menu_func (message: Message):
     await message.answer('Выберите действие из меню', reply_markup=keyboard.menu_kb)
     await message.delete()
 
-@dp.message(Command("info"))
+@dp.message(Command("info")) #Информация о боте 
 async def information(message: Message):
     await message.answer('<b>Добро пожаловать в VandalVlgBot!</b>\n Данный бот служт для того, чтобы предать огласке проблему вандализма\n Разработчики не пропагандируют данное движение, но просят не портить окр.среду\n\n Вандализм уголовно наказуемое преступление, творите добро и приятного пользования ботом!')
     await message.delete()
+
+@dp.message(Command('street')) #Поиск по выбранному району, если район не выбранн по-муолчанию стоит Ворошиловский
+async def street(message: Message):
+    street_area = mass.pop(0)
+    if street_area == 'Центральный':
+        await message.answer('Все доступные объекты по улицам Центрального района', reply_markup=keyboard.area_centr())
+    elif street_area == 'Ворошиловский':
+        await message.answer('Все доступные объекты по улицам Ворошиловского района', reply_markup=keyboard.area_vor())
+    elif street_area == 'Краснооктябрьский':
+        await message.answer('Вы видите все адреса объектов Краснооктябрьского района ', reply_markup=keyboard.area_ko())
+    elif street_area == 'Советский':
+        await message.answer('Вы видите все адреса объектов Советского района ', reply_markup=keyboard.area_sov())
+    elif street_area == 'Кировский':
+        await message.answer('Вы видите все адреса объектов Кировского  района ', reply_markup=keyboard.area_kir())
+    await message.delete()
+
 
 # @dp.message()
 # async def send_photo_file_id(message: Message):
@@ -40,7 +57,7 @@ async def information(message: Message):
 @dp.message() #Принимаем сообщение от пользователя 
 async def echo(message: Message):
     msg = message.text.lower() #Переводим весь текст сообщения в нижний регистр
-
+    area_vibor = area
     if msg == 'районы': #Если пользователь нажал на кнопку "Районы", то
         await message.answer('Вы видите все доступные на данный момент районы города Волгоград', reply_markup=keyboard.area_kb) #Выводим новую клавиатуру с районами
     elif msg == 'удалить объект':
@@ -48,19 +65,25 @@ async def echo(message: Message):
     elif msg == 'добавить объект':
         await message.answer('Вы можете добавить новый объект, подвергшийся действиям вандализма через администратора бота', reply_markup=keyboard.add_delete_kb)
     elif msg == 'ворошиловский':
-        await message.answer(f'Вы видите все адреса объектов района ', reply_markup=keyboard.area_vor())   
+        area_vibor = 'Ворошиловский'
+        mass.insert(0, area_vibor)
+        await message.answer(f'Вы видите все адреса объектов района {area_vibor}', reply_markup=keyboard.area_vor())
     elif msg == 'центральный':
-        area = 'Центральный'
-        await message.answer(f'Вы видите все адреса объектов района {area}', reply_markup=keyboard.area_centr()) 
+        area_vibor = 'Центральный'
+        mass.insert(0, area_vibor)
+        await message.answer(f'Вы видите все адреса объектов района {area_vibor}', reply_markup=keyboard.area_centr()) 
     elif msg == 'краснооктябрьский':
-        area = 'Краснооктябрьский'
-        await message.answer(f'Вы видите все адреса объектов района {area}', reply_markup=keyboard.area_ko())
+        area_vibor = 'Краснооктябрьский'
+        mass.insert(0, area_vibor)
+        await message.answer(f'Вы видите все адреса объектов района {area_vibor}', reply_markup=keyboard.area_ko())
     elif msg == 'советский':
-        area = 'Советский'
-        await message.answer(f'Вы видите все адреса объектов района {area}', reply_markup=keyboard.area_sov())
+        area_vibor = 'Советский'
+        mass.insert(0, area_vibor)
+        await message.answer(f'Вы видите все адреса объектов района {area_vibor}', reply_markup=keyboard.area_sov())
     elif msg == 'кировский':
-        area = 'Кировский'
-        await message.answer(f'Вы видите все адреса объектов района {area}', reply_markup=keyboard.area_kir())
+        area_vibor = 'Кировский'
+        mass.insert(0, area_vibor)
+        await message.answer(f'Вы видите все адреса объектов района {area_vibor}', reply_markup=keyboard.area_kir())
     elif msg == 'назад':
         await message.answer('Выберите район', reply_markup=keyboard.area_kb)
     elif msg == 'благотворительность':
@@ -70,8 +93,12 @@ async def echo(message: Message):
         await message.answer_photo(photo="AgACAgIAAxkBAAICMWUaRToU5QQMrU3mCCShVXteN8ZYAAJ_zjEbcNrRSDIO5-ntIiDVAQADAgADeQADMAQ", caption='Вход')
     elif msg == 'комсомольская 6': 
         await message.answer_photo(photo="AgACAgIAAxkBAAIDZWUk9r94wwbVPNLANubAqpldVZwTAAJ_zTEbwMgpSQx5IVZrlwrPAQADAgADeAADMAQ", caption='Дом')
+    
     else:
         await message.answer('Вы ввели не корректный запрос')
+
+
+
 
         
 async def main():
